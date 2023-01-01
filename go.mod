31e55e62caa571b186a672c041ae75a99d548d98
Andrey Levchenko <108215717+WorkLevchenko@users.noreply.github.com>
2023-01-01 19:22:14 +0300

    Частично разработана модель пользователя.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/WorkLevchenko/willknow
+
+go 1.19
+
+require golang.org/x/crypto v0.4.0 // indirect
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/WorkLevchenko/willknow/users"
+)
+
+func getSighInPage(w http.ResponseWriter, r *http.Request) {
+	templating(w, "sign-in.html", nil)
+}
+
+func getSighUpPage(w http.ResponseWriter, r *http.Request) {
+	templating(w, "sign-up.html", nil)
+}
+
+func templating(w http.ResponseWriter, fileName string, data interface{}) {
+	t, _ := template.ParseFiles(fileName)
+	t.ExecuteTemplate(w, fileName, data)
+}
+
+func signInUser(w http.ResponseWriter, r *http.Request) {
+	newUser := getUser(r)
+	ok := users.DefaultUserService.GetUser(newUser)
+	if !ok {
+		fileName := "sign-in.html"
+		t, _ := template.ParseFiles(fileName)
+		t.ExecuteTemplate(w, fileName, "New user sign in failure.")
+		return
+	}
+	fileName := "sign-in.html"
+	t, _ := template.ParseFiles(fileName)
+	t.ExecuteTemplate(w, fileName, "New user sign in failure.")
+	return
+}
+
+func signUpUser(w http.ResponseWriter, r *http.Request) {
+	newUser := getUser(r)
+	err := users.DefaultUserService.CreateUser(newUser)
+	if err != nil {
+		fileName := "sign-up.html"
+		t, _ := template.ParseFiles(fileName)
+		t.ExecuteTemplate(w, fileName, "New user sign up failure.")
+		return
+	}
+	fileName := "sign-up.html"
+	t, _ := template.ParseFiles(fileName)
+	t.ExecuteTemplate(w, fileName, "New user sign up failure.")
+	return
+}
+
+func getUser(r *http.Request) users.User {
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	return users.User{
+		Email:    email,
+		Password: password,
+	}
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/sign-in":
+		signInUser(w, r)
+	case "/sign-up":
+		signUpUser(w, r)
+	case "/sign-in-form":
+		getSighInPage(w, r)
+	case "/sign-up-form":
+		getSighUpPage(w, r)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", userHandler)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
